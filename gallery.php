414f444d5aa80e0a1e16231448065fe0b32640fa
meryl m <73620120+owl-patia@users.noreply.github.com>
2021-12-16 14:12:41 -0800

    Update gallery.php

---
+++ gallery.php
@@ -0,0 +1,43 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>The Gallery - CSP Project</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/main.css" />
+	</head>
+
+	<body class="left-sidebar is-preload">
+		<div id="page-wrapper" style="background-color:black">
+
+			<!-- Header -->
+            <div id="header">
+
+                <!-- Inner -->
+                    <div class="inner">
+                        <header>
+                            <h1><a href="index.php" id="logo">Gallery</a></h1>
+                        </header>
+                    </div>
+
+                <!-- Nav -->
+                    <nav id="nav">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="gallery.php">Gallery</a></li>
+                            <li><a href="search.php">Search Art</a></li>
+                        </ul>
+                    </nav>
+
+            </div>
+
+            <div id="newcanvas">
+            </div>
+        </div>
+
+    <!--- Scripts -->
+        <script src="assets/script.js"></script>
+        <script src="assets/canvas.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/p5.js"></script>
+	</body>
+</html>
