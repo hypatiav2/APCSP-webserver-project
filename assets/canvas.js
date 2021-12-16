92a7b2898313e460d9d67c01fb0453a07cf72ef0
meryl m <73620120+owl-patia@users.noreply.github.com>
2021-12-16 15:14:54 -0800

    Update canvas.js

--- assets/canvas.js
+++ assets/canvas.js
@@ -1,11 +1,25 @@
-
+let slider;
 function setup() {
-    var myCanvas = createCanvas(1500, 400);
-
-}
-  
-function draw() {
+    var myCanvas = createCanvas(1200, 400);
+    myCanvas.parent("newcanvas");
     background(10);
-    ellipse(50,50,80,80);
-    
+    fill(20);
+    rect(0, 0, 200, height+100);
+    colorPicker = createColorPicker('#ed225d');
+    colorPicker.position(60, height-50);
+    colorPicker.style('background-color', 'black');
+    slider = createSlider(1, 20, 5);
+    slider.position(50, height);
+    slider.style('width', '80px');
+    slider.style('background-color', 'black');
+    slider.style('border', 'solid 1px white');
+    slider.style('height', '2px');
+}
+
+function mouseDragged(){
+    if (mouseX >= 210) {
+        stroke(colorPicker.color());
+        strokeWeight(slider.value());
+        line(mouseX, mouseY, pmouseX, pmouseY);
+    }
 }
